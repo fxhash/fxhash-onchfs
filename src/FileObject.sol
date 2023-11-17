af44c3e30588455bf9e350c1bf2af39650c2b869
steven nevins <steven.nevins.js@gmail.com>
2023-11-17 09:12:43 -0500

    feat: add inode type

--- src/FileObject.sol
+++ src/FileObject.sol
@@ -13,12 +13,18 @@ contract FileObject {
     // Define the structure of a directory
     struct Directory {
         string[] names;
-        bytes32[] chunkPointersHash;
+        bytes32[] inodePointers;
+    }
+
+    // Define the enumeration for inode types
+    enum InodeType {
+        File,
+        Directory
     }
 
     // Define the structure of an inode
     struct Inode {
-        bool isFile;
+        InodeType inodeType;
         File file;
         Directory directory;
     }
@@ -35,13 +41,13 @@ contract FileObject {
 
     // Function to create a file
     function createFile(bytes memory metadata, bytes32[] memory chunkPointers) public {
-        bytes32 metadataHash = keccak256(metadata);
-        bytes32 chunkPointersHash = keccak256(abi.encode(chunkPointers));
-        bytes32 checksum = keccak256(abi.encode(metadataHash, chunkPointersHash, bytes1(0x01)));
+        bytes32 checksum = keccak256(
+            abi.encode(keccak256(abi.encode(chunkPointers)), keccak256(abi.encode(metadata)), bytes1(0x01))
+        );
         require(!inodeExists(checksum), "Inode already exists");
 
         File memory newFile = File(metadata, chunkPointers);
-        inodes[checksum] = Inode(true, newFile, Directory(new string[](0), new bytes32[](0)));
+        inodes[checksum] = Inode(InodeType.File, newFile, Directory(new string[](0), new bytes32[](0)));
     }
 
     // Function to create a directory
@@ -51,7 +57,8 @@ contract FileObject {
         bytes32 checksum = keccak256(abi.encode(names, inodePointers, bytes1(0x00)));
         require(!inodeExists(checksum), "Inode already exists");
 
-        inodes[checksum] = Inode(false, File("", new bytes32[](0)), Directory(names, inodePointers));
+        Directory memory newDirectory = Directory(names, inodePointers);
+        inodes[checksum] = Inode(InodeType.Directory, File("", new bytes32[](0)), newDirectory);
     }
 
     // Function to check if an inode exists
@@ -59,6 +66,8 @@ contract FileObject {
         // In Solidity, an uninitialized struct has all its fields set to their default values
         // So, we can check if a file or directory exists by checking if both its fields are default
         Inode memory inode = inodes[checksum];
-        return inode.isFile || (inode.file.metadata.length != 0 || inode.file.chunkPointers.length != 0);
+        return
+            inode.inodeType == InodeType.File ||
+            (inode.file.metadata.length != 0 || inode.file.chunkPointers.length != 0);
     }
 }
