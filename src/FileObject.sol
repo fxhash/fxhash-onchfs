12966db55fc5c416b8424934292fed2792b359a4
steven nevins <steven.nevins.js@gmail.com>
2023-11-17 10:23:04 -0500

    chore: refactor functionality

--- src/FileObject.sol
+++ src/FileObject.sol
@@ -5,77 +5,73 @@ import {ContentStore} from "ethfs/packages/contracts/src/ContentStore.sol";
 import {SSTORE2} from "sstore2/SSTORE2.sol";
 
 contract FileObject {
-    // Define the structure of a file
     struct File {
         bytes metadata;
         bytes32[] chunkPointers;
     }
 
-    // Define the structure of a directory
     struct Directory {
         string[] names;
         bytes32[] inodePointers;
     }
 
-    // Define the enumeration for inode types
     enum InodeType {
         File,
         Directory
     }
 
-    // Define the structure of an inode
     struct Inode {
         InodeType inodeType;
         File file;
         Directory directory;
     }
 
-    // Forbidden characters
     string private constant FORBIDDEN_CHARS = ":/?#[]@!$&'()*+,;=";
 
-    // Define the mapping of inodes
     mapping(bytes32 => Inode) internal inodes;
-
-    // Instance of the ContentStore contract
     ContentStore public contentStore;
 
     constructor(address _contentStore) {
         contentStore = ContentStore(_contentStore);
     }
 
-    // Function to create a file
     function createFile(bytes memory metadata, bytes32[] memory chunkPointers) public {
         verifyNotIncludes(string(metadata), FORBIDDEN_CHARS);
-        bytes memory allChunkPointers = abi.encodePacked(chunkPointers);
 
-        bytes32 checksum = keccak256(abi.encodePacked(bytes1(0x01), keccak256(allChunkPointers), keccak256(metadata)));
+        bytes32 checksum = keccak256(
+            bytes.conat(bytes1(0x01), keccak256(bytes.concat(chunkPointers)), keccak256(metadata))
+        );
         require(!inodeExists(checksum), "Inode already exists");
-
         File memory newFile = File(metadata, chunkPointers);
         inodes[checksum] = Inode(InodeType.File, newFile, Directory(new string[](0), new bytes32[](0)));
     }
 
-    // Function to create a directory
     function createDirectory(string[] memory names, bytes32[] memory inodePointers) public {
-        uint256 length = names.length;
-        require(length == inodePointers.length, "Mismatch in lengths of names and inodePointers");
-        bytes32[] memory hashedNames = new bytes32[](length);
-        for (uint256 i; i < length; i++) {
-            verifyNotIncludes(names[i], FORBIDDEN_CHARS);
-            hashedNames[i] = keccak256(bytes(names[i]));
-        }
+        require(names.length == inodePointers.length, "Mismatch in lengths of names and inodePointers");
+        bytes32[] memory hashedNames = hashNames(names);
 
-        bytes memory allNames = abi.encodePacked(hashedNames);
-        bytes memory allInodePointers = abi.encodePacked(inodePointers);
-
-        bytes32 checksum = keccak256(abi.encodePacked(bytes1(0x00), keccak256(allNames), keccak256(allInodePointers)));
+        bytes32 checksum = keccak256(
+            bytes.conat(bytes1(0x00), keccak256(bytes.concat(hashedNames)), keccak256(bytes.concat(inodePointers)))
+        );
         require(!inodeExists(checksum), "Inode already exists");
-
         Directory memory newDirectory = Directory(names, inodePointers);
         inodes[checksum] = Inode(InodeType.Directory, File("", new bytes32[](0)), newDirectory);
     }
 
-    // Function to check if an inode exists
+    function readFile(bytes32 checksum) public view returns (bytes memory) {
+        require(inodeExists(checksum), "Inode not found");
+        Inode memory inode = inodes[checksum];
+        require(inode.inodeType == InodeType.File, "Inode is not a file");
+        return concatenateChunks(inode.file.chunkPointers);
+    }
+
+    function readDirectory(bytes32 checksum) public view returns (string[] memory, bytes32[] memory) {
+        require(inodeExists(checksum), "Inode not found");
+        Inode memory inode = inodes[checksum];
+        require(inode.inodeType == InodeType.Directory, "Inode is not a directory");
+        return (inode.directory.names, inode.directory.inodePointers);
+    }
+
     function inodeExists(bytes32 checksum) public view returns (bool) {
         Inode memory inode = inodes[checksum];
         if (inode.inodeType == InodeType.File) {
@@ -85,38 +81,37 @@ contract FileObject {
         }
     }
 
-    function readFile(bytes32 checksum) public view returns (bytes memory) {
-        require(inodeExists(checksum), "Inode not found");
-        Inode memory inode = inodes[checksum];
-        require(inode.inodeType == InodeType.File, "Inode is not a file");
-
-        bytes memory fileContent;
-        bytes32 chunkChecksum;
-        address pointer;
-        bytes memory chunkContent;
-        for (uint256 i = 0; i < inode.file.chunkPointers.length; i++) {
-            chunkChecksum = inode.file.chunkPointers[i];
-            pointer = contentStore.getPointer(chunkChecksum);
-            chunkContent = SSTORE2.read(pointer);
-            fileContent = abi.encodePacked(fileContent, chunkContent);
-        }
-
-        return fileContent;
-    }
-
-    function readDirectory(bytes32 checksum) public view returns (string[] memory, bytes32[] memory) {
-        require(inodeExists(checksum), "Inode not found");
-        Inode memory inode = inodes[checksum];
-        require(inode.inodeType == InodeType.Directory, "Inode is not a directory");
-
-        return (inode.directory.names, inode.directory.inodePointers);
-    }
-
     function verifyNotIncludes(string memory str, string memory charset) private pure {
         for (uint256 i = 0; i < bytes(str).length; i++) {
             for (uint256 j = 0; j < bytes(charset).length; j++) {
                 require(bytes(str)[i] != bytes(charset)[j], "String contains a forbidden character");
             }
         }
+    }
+
+    function calculateChecksum(bytes memory data1, bytes memory data2) private pure returns (bytes32) {
+        bytes memory allData = abi.encodePacked(data1, data2);
+        return keccak256(allData);
+    }
+
+    function hashNames(string[] memory names) private pure returns (bytes32[] memory) {
+        uint256 length = names.length;
+        bytes32[] memory hashedNames = new bytes32[](length);
+        for (uint256 i; i < length; i++) {
+            verifyNotIncludes(names[i], FORBIDDEN_CHARS);
+            hashedNames[i] = keccak256(bytes(names[i]));
+        }
+        return hashedNames;
+    }
+
+    function concatenateChunks(bytes32[] memory chunkPointers) private view returns (bytes memory) {
+        bytes memory fileContent;
+        for (uint256 i = 0; i < chunkPointers.length; i++) {
+            bytes32 chunkChecksum = chunkPointers[i];
+            address pointer = contentStore.getPointer(chunkChecksum);
+            bytes memory chunkContent = SSTORE2.read(pointer);
+            fileContent = abi.encodePacked(fileContent, chunkContent);
+        }
+        return fileContent;
     }
 }
