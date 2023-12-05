75eb8c104c7919500343ccbee2ac158384da1840
Sahil Mehta <samehta91@gmail.com>
2023-12-05 13:39:40 -0800

    chore: add natspec to structs

--- src/lib/Structs.sol
+++ src/lib/Structs.sol
@@ -5,21 +5,42 @@ pragma solidity 0.8.23;
                                 STRUCTS
 //////////////////////////////////////////////////////////////////////////*/
 
+/**
+ * @notice Type of inode
+ * - `Directory` List of inodes with their assigned names
+ * - `File` Single inode with assigned name
+ */
 enum InodeType {
     Directory,
     File
 }
 
+/**
+ * @notice Struct of directory information
+ * - `filenames` List of filenames
+ * - `fileChecksums` List of checksums for each file
+ */
 struct Directory {
     string[] filenames;
     bytes32[] fileChecksums;
 }
 
+/**
+ * @notice Struct of file information
+ * - `metadata` Bytes-encoded metadata of file
+ * - `chunkChecksums` List of chunked checksums
+ */
 struct File {
     bytes metadata;
     bytes32[] chunkChecksums;
 }
 
+/**
+ * @notice Struct of inode information
+ * - `inodeType` Type of inode
+ * - `file` Struct of file information
+ * - `directory` Struct of directory information
+ */
 struct Inode {
     InodeType inodeType;
     File file;
